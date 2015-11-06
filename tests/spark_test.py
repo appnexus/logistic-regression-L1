bfdb9ed2a8ac2828725dcddf27d852c83eecda95
Steph-O <stzeng@appnexus.com>
2015-11-06 16:07:26 +0000

    added test cases

---
+++ tests/spark_test.py
@@ -0,0 +1,10 @@
+"""
+Make sure all of the spark parts work!
+Test cases to cover:
+    Force same data from convergence tests into spark
+    Take input of both list of lists and nparray
+    Ensure that the result of `fit` ~= pre-stated betas
+    Check that prediction of test data = y / m
+    Check betas and intercepts with zeros
+    Anything else??
+"""
