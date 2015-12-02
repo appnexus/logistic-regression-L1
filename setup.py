39270bf89f9af8d830dadf6ac3eb01b6029814fa
Moussa Taifi <mtaifi@appnexus.com>
2015-12-02 16:01:42 -0500

    update the crew's names

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='logistic_regression_L1',
     version='0.1',
-    author='Stephanie Tzeng',
-    author_email='stzeng@appnexus.com',
+    author='Stephanie Tzeng, Sandesh Devaraju, Umar Aftab, Moussa Taifi, Abraham Greenstein',
+    author_email='stzeng@appnexus.com, sdevaraju@appnexus.com, uaftab@appnexus.com, mtaifi@appnexus.com, agreenstein@appnexus.com',
     packages=['logistic_regression_L1'],
     url='https://github.com/appnexus/logistic_regression_L1',
     description='Logistic Regression with L1 Penalty',
